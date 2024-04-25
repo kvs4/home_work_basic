6af7ca06dbefe12e98d61cb6918bc8dc6f29a4b1
vboxuser <vboxuser@MyUbuntu.myguest.virtualbox.org>
2024-04-25 16:56:12 +0300

    fixed linter's remarks

--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -1,11 +1,14 @@
 package reader
 
-import "encoding/json"
-import "fmt"
-import "io"
-import "os"
+import (
+	"encoding/json"
 
-import "github.com/fixme_my_friend/hw02_fix_app/types"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/fixme_my_friend/hw02_fix_app/types"
+)
 
 func ReadJSON(filePath string) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
@@ -22,6 +25,10 @@ func ReadJSON(filePath string) ([]types.Employee, error) {
 	var data []types.Employee
 
 	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return nil, nil
+	}
 
 	res := data
 
