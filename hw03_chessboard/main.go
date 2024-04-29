56130845a08ff14f88270f67eac2bfa0590da93a
vboxuser <vboxuser@MyUbuntu.myguest.virtualbox.org>
2024-04-30 00:06:34 +0300

    hw03 is complited

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	numberLineX := 8
+	numberLineY := 8
+
+	for i := 0; i < numberLineY; i++ {
+		lineX := ""
+		evenUnEven := 0
+		if i%2 == 0 {
+			evenUnEven = 0
+		} else {
+			evenUnEven = 1
+		}
+		for j := 0; j < numberLineX; j++ {
+			if j%2 == evenUnEven {
+				lineX += " "
+			} else {
+				lineX += "#"
+			}
+		}
+		fmt.Println(lineX)
+	}
 }
