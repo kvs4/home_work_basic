3915a32a3f79dafa0d3682f12d3de64a3da29420
kvs4 <165914075+kvs4@users.noreply.github.com>
2024-06-13 19:39:10 +0300

    Hw07 word counter (#8)
    hw07 is complited

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
 func main() {
-	// Place your code here.
+	sample := "five, five, five one.. two: three/// one"
+	wordCounts := countWords(sample)
+	for word, count := range wordCounts {
+		fmt.Printf("%s: %d\n", word, count)
+	}
+}
+
+func countWords(text string) map[string]int {
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c)
+	}
+	words := strings.FieldsFunc(text, f)
+	wordCounts := make(map[string]int)
+
+	for _, word := range words {
+		word = strings.ToLower(word) // conversion the word to lowercase
+		wordCounts[word]++
+	}
+	return wordCounts
 }
