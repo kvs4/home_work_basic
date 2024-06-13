2f576e809da48f05f52beff17175bbd79ccff2a3
kvs4 <165914075+kvs4@users.noreply.github.com>
2024-06-13 19:37:50 +0300

    Hw05 shapes (#6)
    hw05 is complited

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -1,5 +1,88 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+}
+
+type circle struct {
+	radius int
+}
+
+func (c *circle) Area() float64 {
+	r := float64(c.radius)
+	return math.Pi * math.Pow(r, 2.0)
+}
+
+type triangle struct {
+	base, height int
+}
+
+func (t *triangle) Area() float64 {
+	return 0.5 * float64(t.base) * float64(t.height)
+}
+
+type rectangle struct {
+	width, height int
+}
+
+func (r *rectangle) Area() float64 {
+	return float64(r.width) * float64(r.height)
+}
+
+func calculateArea(s any) (float64, error) {
+	sh, ok := s.(Shape)
+	if !ok {
+		textError := errors.New("this type of shape isn't available")
+		return 0.0, textError
+	}
+
+	return sh.Area(), nil
+}
+
 func main() {
-	// Place your code here.
+	var (
+		area float64
+		err  error
+	)
+
+	myCircle := &circle{5}
+	area, err = calculateArea(myCircle)
+	if err != nil {
+		log.Println("error:", err)
+		// return
+	}
+	fmt.Println("Circle: radius", myCircle.radius)
+	fmt.Println("The area of the circle:", area)
+
+	myRectangle := &rectangle{10, 5}
+	area, err = calculateArea(myRectangle)
+	if err != nil {
+		log.Println("error:", err)
+		// return
+	}
+	fmt.Println("Rectangle: width", myRectangle.width, ", height", myRectangle.height)
+	fmt.Println("The area of the rectangle:", area)
+
+	myTriangle := &triangle{8, 6}
+	area, err = calculateArea(myTriangle)
+	if err != nil {
+		log.Println("error:", err)
+		// return
+	}
+	fmt.Println("Triangle: base", myTriangle.base, ", height", myTriangle.height)
+	fmt.Println("The area of the triangle:", area)
+
+	notShape := "not a shape"
+	_, err = calculateArea(notShape)
+	if err != nil {
+		log.Println("error:", err)
+		// return
+	}
 }
