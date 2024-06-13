c297862be4f876956b0011708a42b4f1601dc508
kvs4 <165914075+kvs4@users.noreply.github.com>
2024-06-13 19:35:28 +0300

    Hw03 chessboard (#4)
    hw03 is complited

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	// Place your code here.
+	var numberLineX, numberLineY int
+	fmt.Print("Write the number of horizontal lines: ")
+	_, err := fmt.Scan(&numberLineX)
+	if err != nil {
+		fmt.Println("scanning error")
+		return
+	}
+	fmt.Print("Write the number of vertical lines: ")
+	_, err = fmt.Scan(&numberLineY)
+	if err != nil {
+		fmt.Println("scanning error")
+		return
+	}
+
+	for i := 0; i < numberLineY; i++ {
+		var lineX strings.Builder
+		evenUnEven := 0
+		if i%2 == 0 {
+			evenUnEven = 0
+		} else {
+			evenUnEven = 1
+		}
+		for j := 0; j < numberLineX; j++ {
+			if j%2 == evenUnEven {
+				lineX.WriteString(" ")
+			} else {
+				lineX.WriteString("#")
+			}
+		}
+		fmt.Println(lineX.String())
+	}
 }
