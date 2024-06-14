af1aadaf1cd430570955ebaea220c1107c0c6e79
kvs4 <165914075+kvs4@users.noreply.github.com>
2024-06-14 19:48:29 +0300

    hw08 is complited (#9)
    Co-authored-by: vboxuser <vboxuser@MyUbuntu.myguest.virtualbox.org>

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+)
+
+func BinarySearch(arr []int, target int) int {
+	left, right := 0, len(arr)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if arr[mid] == target {
+			return mid
+		}
+
+		if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
+
 func main() {
-	// Place your code here.
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	target := 9
+	result := BinarySearch(arr, target)
+	if result != -1 {
+		fmt.Printf("Element found at index %d\n", result)
+	} else {
+		fmt.Println("Element not found")
+	}
 }
