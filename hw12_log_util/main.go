614a56dde83c8229dd57e328e44c0bdad5397e95
vboxuser <vboxuser@MyUbuntu.myguest.virtualbox.org>
2024-07-28 01:21:48 +0300

    hw12 is complited

--- hw12_log_util/main.go
+++ hw12_log_util/main.go
@@ -1,5 +1,128 @@
 package main
 
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/pflag"
+)
+
+type settings struct {
+	fileName, level, outputFile string
+}
+
 func main() {
-	// Place your code here.
+	logsSettings := getSettings()
+
+	logs, err := ReadLogFile(logsSettings.fileName)
+	if err != nil {
+		log.Fatal()
+	}
+	foundLogs := AnalizeTheLog(logs, logsSettings.level)
+	WriteOutputFile(foundLogs, logsSettings.outputFile)
+}
+
+func getSettings() settings {
+	var fileName, level, outputFile string
+	pflag.StringVarP(&fileName, "file", "f", "", "указывает путь к анализируемому лог-файлу")
+	pflag.StringVarP(&level, "level", "l", "", "указывает уровень логов для анализа")
+	pflag.StringVarP(&outputFile, "output", "o", "", "указывает путь к файлу, в который будет записана статистика")
+	configPath := pflag.String("config", ".env", "path to config file")
+	pflag.Parse()
+
+	fileNameEnv := os.Getenv("LOG_ANALYZER_FILE")
+	levelEnv := os.Getenv("LOG_ANALYZER_LEVEL")
+	outputEnv := os.Getenv("LOG_ANALYZER_OUTPUT")
+
+	err := godotenv.Load(*configPath)
+	if err != nil {
+		log.Printf("Can't parse config: %v \n", err)
+	}
+
+	fileName = checkFlagAndPullFromConfigFile(fileName, fileNameEnv, "LOG_ANALYZER_FILE")
+	level = checkFlagAndPullFromConfigFile(level, levelEnv, "LOG_ANALYZER_LEVEL")
+	outputFile = checkFlagAndPullFromConfigFile(outputFile, outputEnv, "LOG_ANALYZER_OUTPUT")
+
+	logsSettings := settings{
+		fileName:   fileName,
+		level:      level,
+		outputFile: outputFile,
+	}
+	return logsSettings
+}
+
+func checkFlagAndPullFromConfigFile(flag string, variableEnv string, kindVarEnv string) string {
+	if flag == "" && variableEnv == "" {
+		variableConfEnv, ok := os.LookupEnv(kindVarEnv)
+		if ok {
+			flag = variableConfEnv
+		} else if kindVarEnv == "LOG_ANALYZER_FILE" {
+			log.Fatalf("The name of the log file is not specified. It must be specified using the -f (or --file) flag, either in the environment variable %s or in the \".env\" configuration file\n", kindVarEnv)
+		}
+	} else if flag == "" {
+		flag = variableEnv
+	}
+	return flag
+}
+
+func ReadLogFile(fileName string) ([][]string, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Printf("Can't open file \"%s\": %v", fileName, err)
+		return nil, err
+	}
+	defer file.Close()
+
+	fileReader := csv.NewReader(file)
+	lines, err := fileReader.ReadAll()
+	if err != nil {
+		log.Printf("Can't read file \"%s\": %v", fileName, err)
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+func AnalizeTheLog(logs [][]string, level string) []string {
+	var foundLogs []string
+	for _, log := range logs {
+		if log[0] == level {
+			for j, elem := range log {
+				if j == 0 && elem != level {
+					break
+				} else if j > 0 {
+					foundLogs = append(foundLogs, elem)
+				}
+			}
+		}
+	}
+	return foundLogs
+}
+
+func WriteOutputFile(foundLogs []string, outputFile string) {
+	var (
+		file *os.File
+		w    *bufio.Writer
+		err  error
+	)
+
+	if outputFile == "" {
+		file = os.Stdout
+	} else {
+		file, err = os.Create(outputFile)
+		if err != nil {
+			log.Printf("Can't create output file \"%s\": %v", outputFile, err)
+		}
+		defer file.Close()
+	}
+
+	w = bufio.NewWriter(file)
+	for _, log := range foundLogs {
+		w.Write([]byte(fmt.Sprintf("%s\n", log)))
+	}
+	w.Flush()
 }
---
+++ hw12_log_util/main_test.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadLogFile(t *testing.T) {
+	wantLogs := [][]string{
+		{"error", "a file system error has occurred"},
+		{"info", "a USB device is connected"},
+		{"info", "a USB device is disconnected"},
+		{"warning", "a suspicious file has been found"},
+		{"warning", "a suspicious file has been found"},
+		{"warning", "a suspicious file has been found"},
+	}
+	gotlogs, err := ReadLogFile("log_test.csv")
+	require.NoError(t, err)
+	assert.Equal(t, wantLogs, gotlogs)
+}
+
+func TestAnalizeTheLog(t *testing.T) {
+	logs := [][]string{
+		{"error", "a file system error has occurred"},
+		{"info", "a USB device is connected"},
+		{"info", "a USB device is disconnected"},
+		{"warning", "a suspicious file has been found"},
+		{"warning", "a suspicious file has been found"},
+		{"warning", "a suspicious file has been found"},
+	}
+
+	wantLogs := []string{
+		"a file system error has occurred",
+	}
+	gotLogs := AnalizeTheLog(logs, "error")
+	assert.Equal(t, wantLogs, gotLogs)
+}
+
+func TestAnalizeTheLogEmpty(t *testing.T) {
+	var wantLogs []string
+	logs := [][]string{}
+
+	gotLogs := AnalizeTheLog(logs, "error")
+	assert.Equal(t, wantLogs, gotLogs)
+}
+
+func TestWriteOutputFile(t *testing.T) {
+	fileName := "output_test.txt"
+	logs := []string{
+		"a file system error has occurred",
+	}
+	WriteOutputFile(logs, fileName)
+
+	file, err := os.Open(fileName)
+	require.NoError(t, err, "Can't open output file \"%s\": %v", fileName, err)
+	defer file.Close()
+
+	b, err := io.ReadAll(file)
+	require.NoError(t, err, "Can't read output file \"%s\": %v", fileName, err)
+
+	lines := bytes.Split(b, []byte("\n"))
+	for i, wantLog := range logs {
+		gotLog := string(lines[i])
+		assert.Equal(t, wantLog, gotLog)
+	}
+}
+
+/*func TestWriteOutputFileEmpty(t *testing.T) {
+	var gotLogs []string
+	fileName := ""
+	logs := []string{
+		"a file system error has occurred",
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		reader := bufio.NewReader(os.Stdout)
+		fmt.Println("wait 3 sec")
+		time.Sleep(time.Second * 3)
+		for {
+			line, err := reader.ReadString('\n')
+			fmt.Println(line)
+			if err == io.EOF {
+				fmt.Println("break, line = ", line)
+				break
+			} else {
+				require.NoError(t, err, "Can't read os.Stdout: %v", err)
+				gotLogs = append(gotLogs, line)
+				fmt.Println("line = ", line)
+				fmt.Printf("gotLogs = %#v", gotLogs)
+			}
+		}
+		fmt.Println("wg.Done()")
+		wg.Done()
+	}()
+
+	fmt.Println("wait 1 sec")
+	time.Sleep(time.Millisecond * 1000)
+	fmt.Println("start WriteOutputFile")
+	WriteOutputFile(logs, fileName)
+
+	fmt.Println("wg.Wait()")
+	wg.Wait()
+
+	assert.Equal(t, logs, gotLogs)
+}*/
+
+/*func TestWriteOutputFileEmpty(t *testing.T) {
+	var gotLogs []string
+	fileName := ""
+	logs := []string{
+		"a file system error has occurred",
+	}
+
+	//wg := sync.WaitGroup{}
+	//wg.Add(1)
+	//go func() {
+
+	//	wg.Done()
+	//}()
+
+	//fmt.Println("wait 1 sec")
+	//time.Sleep(time.Millisecond * 1000)
+	fmt.Println("start WriteOutputFile")
+	WriteOutputFile(logs, fileName)
+
+	reader := bufio.NewReader(os.Stdin)
+	//fmt.Println("wait 2 sec")
+	time.Sleep(time.Second * 2)
+	for {
+		line, err := reader.ReadString('\n')
+		fmt.Println(line)
+		if err == io.EOF {
+			fmt.Println("break, line = ", line)
+			break
+		} else {
+			require.NoError(t, err, "Can't read os.Stdout: %v", err)
+			gotLogs = append(gotLogs, line)
+			fmt.Println("line = ", line)
+			fmt.Printf("gotLogs = %#v", gotLogs)
+		}
+	}
+	//fmt.Println("wg.Done()")
+
+	//fmt.Println("wg.Wait()")
+	//wg.Wait()
+
+	assert.Equal(t, logs, gotLogs)
+}*/
