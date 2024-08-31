2f521866e49ea2ea3ec4e51abb94e45a143cdf1c
vboxuser <vboxuser@MyUbuntu.myguest.virtualbox.org>
2024-08-31 23:11:43 +0300

    fixed tests

--- hw13_http/client/client_test.go
+++ hw13_http/client/client_test.go
@@ -2,11 +2,23 @@ package client
 
 import (
 	"testing"
+	"time"
 
+	"github.com/kvs4/home_work_basic/hw13_http/server"
 	"github.com/stretchr/testify/assert"
 )
 
+func runServer() {
+	go func() {
+		server.Run()
+	}()
+
+	time.Sleep(time.Second * 2)
+}
+
 func TestMakeGetRequest(t *testing.T) {
+	runServer()
+
 	wantbody := `"this is method GET"`
 	url := "http://0.0.0.0:8080/v1/"
 
@@ -20,6 +32,8 @@ func TestMakeGetRequest(t *testing.T) {
 }
 
 func TestMakeGPostRequest(t *testing.T) {
+	runServer()
+
 	wantbody := `"this is method POST"`
 	url := "http://0.0.0.0:8080/v1/"
 
